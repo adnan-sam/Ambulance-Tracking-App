13596b2689ab85ddf38783c4e557b04c054ccea8
Adnan Sameer <adnansameer400@gmail.com>
2022-07-12 14:18:26 +0530

    create contact.html

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-const express=require("express");
+// const express=require("express");
 
 
-const app=express();
+// const app=express();
 
-app.get("/",function(res,req){
-    res.prependListener("index.html");
-})
+// app.get("/",function(res,req){
+//     res.prependListener("index.html");
+// })
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
-})
+// app.listen(3000, function(){
+//     console.log("Server is running on port 3000");
+// })
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
