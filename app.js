50aa1e5bba58a4ef0ff2cae8052ec03e7ad34eb1
Adnan Sameer <adnansameer400@gmail.com>
2022-07-13 20:32:03 +0530

    Add login page

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
-// const express=require("express");
+const express=require("express");
 
 
-// const app=express();
+const app=express();
 
-// app.get("/",function(res,req){
-//     res.prependListener("index.html");
-// })
+app.get("/2ndpg",function(res,req){
+  
+  $('#verify-otp').hide();
+  $('#request-otp').on('click',function(){
+    $('#sign-in').hide();
+    $('#verify-otp').show();
+  });
+  $('.fa-chevron-left').on('click',function(){
+    $('#sign-in').show();
+    $('#verify-otp').hide();
+  });
+  res.render("2ndpg.html");
+})
 
-// app.listen(3000, function(){
-//     console.log("Server is running on port 3000");
-// })
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+})
 
-const inputs = document.querySelectorAll(".input");
 
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
+// const inputs = document.querySelectorAll(".input");
 
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
+// function focusFunc() {
+//   let parent = this.parentNode;
+//   parent.classList.add("focus");
+// }
 
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
-});
+// function blurFunc() {
+//   let parent = this.parentNode;
+//   if (this.value == "") {
+//     parent.classList.remove("focus");
+//   }
+// }
+
+// inputs.forEach((input) => {
+//   input.addEventListener("focus", focusFunc);
+//   input.addEventListener("blur", blurFunc);
+// });
